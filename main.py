c0b3ec8a8567a58ee932ba6dc06e44ffb9e673ba
CT-42210 <nicholastroiano123@gmail.com>
2023-01-10 15:46:23 -0500

    thingy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import cv2
 
-# Create a HOG descriptor
-hog = cv2.HOGDescriptor()
-
-# Set the SVM (Support Vector Machine) detector to detect humans
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
 # Open the video capture
 cap = cv2.VideoCapture(0)
 
@@ -36,35 +30,29 @@ while True:
     # Find contours in the mask
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Check if any contours have a large enough area to be considered motion
-    motion = False
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-        if w > 50 and h > 50:
-            motion = True
+    # Extract bounding boxes from the contours
+    boxes = [cv2.boundingRect(contour) for contour in contours]
+
+    # Apply non-maximum suppression to merge overlapping boxes
+    confidence_scores = [1.0] * len(boxes)
+    confidence_threshold = 0.5
+    overlap_threshold = 0.5
+    boxes = cv2.dnn.NMSBoxes(boxes, confidence_scores, confidence_threshold, overlap_threshold)
+
+    # Draw the bounding box on the frame
+    for box in boxes:
+        x, y, w, h = box
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # Display the resulting frame
+        cv2.imshow("Motion Detection", frame)
+
+        # Wait for the user to press a key
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # If motion was detected, check for humans in the next frame
-    if motion:
-        # Detect humans in the next frame
-        humans, _ = hog.detectMultiScale(next_frame)
+        # Release the video capture
+    cap.release()
 
-        # Draw a bounding box around each detected human
-        for (x, y, w, h) in humans:
-            cv2.rectangle(next_frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-    # Display the resulting frame
-    cv2.imshow("Human Detection", next_frame)
-
-    # Set the current frame to the next frame
-    frame = next_frame
-
-    # Wait for the user to press a key
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the video capture
-cap.release()
-
-# Destroy all windows
-cv2.destroyAllWindows()
+    # Destroy all windows
+    cv2.destroyAllWindows()
