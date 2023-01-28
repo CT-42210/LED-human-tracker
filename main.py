d86acae31b97544121cb93ea41a72c8008077c1e
CT-42210 <nicholastroiano123@gmail.com>
2023-01-27 22:16:50 -0500

    tensorflow?

--- main.py
+++ main.py
@@ -1,45 +1,40 @@
+import tensorflow as tf
 import cv2
-import numpy as np
-import imutils
 
-# Start capturing the video
+# Load TensorFlow Lite model
+interpreter = tf.lite.Interpreter(model_path="lite-model_efficientdet_lite0_detection_default_1.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# OpenCV video capture
 cap = cv2.VideoCapture(0)
 
-# Create an empty image to store the background
-bg = None
-
-# Loop through the first 30 frames
-for i in range(30):
-    ret, frame = cap.read()
-    if bg is None:
-        bg = frame.copy().astype("float")
-    else:
-        cv2.accumulateWeighted(frame, bg, 0.5)
-
 while True:
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    frame_delta = cv2.absdiff(frame, cv2.convertScaleAbs(bg))
 
-    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+    # Pre-processing: resize and normalize
+    frame = cv2.resize(frame, (192, 192))
+    frame = frame / 255.0
 
-    kernel = np.ones((3, 3), np.uint8)
-    thresh = cv2.dilate(thresh, kernel, iterations=2)
-    gray = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
-    cnts = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Run inference
+    interpreter.set_tensor(input_details[0]['index'], [frame])
+    interpreter.invoke()
+    boxes = interpreter.get_tensor(output_details[0]['index'])
 
-    cnts = imutils.grab_contours(cnts)
-    args = {"min_area": 500}
+    # Draw bounding boxes on the frame
+    for box in boxes:
+        xmin, ymin, xmax, ymax = box
+        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
 
-    for c in cnts:
-        if cv2.contourArea(c) < args["min_area"]:
-            continue
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Display the resulting frame
+    cv2.imshow('Human Detection', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        cv2.imshow("frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
-
+# Release video capture
 cap.release()
 cv2.destroyAllWindows()
