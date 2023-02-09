79f0c702212d12c56b9d7ab63de694dd9d524e08
CT-42210 <nicholastroiano123@gmail.com>
2023-02-09 11:13:49 -0500

    i have no clue

--- motion.py
+++ motion.py
@@ -49,21 +49,15 @@ while video.isOpened():
     thresh_frame = cv2.threshold(diff_frame, thresh_int, 255, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
-    # Copy the thresholded image.
     im_floodfill = thresh_frame.copy()
 
-    # Mask used to flood filling.
-    # Notice the size needs to be 2 pixels than the image.
     h, w = thresh_frame.shape[:2]
     mask = np.zeros((h + 2, w + 2), np.uint8)
 
-    # Floodfill from point (0, 0)
     cv2.floodFill(im_floodfill, mask, (0, 0), 255);
 
-    # Invert floodfilled image
     im_floodfill_inv = cv2.bitwise_not(im_floodfill)
 
-    # Combine the two images to get the foreground.
     im_out = thresh_frame | im_floodfill_inv
 
     display_frame = frame2.copy()
