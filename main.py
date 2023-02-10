0af11078db00ef2e76d56420d5a062ca470c6f3d
CT-42210 <nicholastroiano123@gmail.com>
2023-02-10 12:20:19 -0500

    tensorflow funny

--- main.py
+++ main.py
@@ -196,8 +196,8 @@ while True:
 
             # Draw label
             object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
-            label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
-            if label == 'person':
+            if object_name == 'person':
+                label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
                 labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
                 label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
                 cv2.rectangle(frame, (xmin, label_ymin - labelSize[1] - 10),
