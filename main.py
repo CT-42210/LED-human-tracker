6b7ca3048dd8e381a10c97201a4e69c732102095
CT-42210 <nicholastroiano123@gmail.com>
2023-02-14 13:27:56 -0500

    random things

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import argparse
 import cv2
 import numpy as np
-import sys
 import time
 from threading import Thread
 import importlib.util
@@ -48,7 +47,6 @@ class VideoStream:
         self.stopped = True
 
 
-# Define and parse input arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
                     required=True)
@@ -58,8 +56,8 @@ parser.add_argument('--labels', help='Name of the labelmap file, if different th
                     default='labelmap.txt')
 parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
                     default=0.5)
-parser.add_argument('--resolution',
-                    help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
+parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the '
+                                         'resolution entered, errors may occur.',
                     default='1280x720')
 parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
                     action='store_true')
@@ -74,37 +72,24 @@ resW, resH = args.resolution.split('x')
 imW, imH = int(resW), int(resH)
 use_TPU = args.edgetpu
 
-# Import TensorFlow libraries
-# If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
-# If using Coral Edge TPU, import the load_delegate library
 pkg = importlib.util.find_spec('tflite_runtime')
 if pkg:
     from tflite_runtime.interpreter import Interpreter
-
     if use_TPU:
         from tflite_runtime.interpreter import load_delegate
 else:
     from tensorflow.lite.python.interpreter import Interpreter
-
     if use_TPU:
         from tensorflow.lite.python.interpreter import load_delegate
 
-# If using Edge TPU, assign filename for Edge TPU model
 if use_TPU:
-    # If user has specified the name of the .tflite file, use that name, otherwise use default 'edgetpu.tflite'
-    if (GRAPH_NAME == 'detect.tflite'):
+    if GRAPH_NAME == 'detect.tflite':
         GRAPH_NAME = 'edgetpu.tflite'
 
-    # Get path to current working directory
 CWD_PATH = os.getcwd()
-
-# Path to .tflite file, which contains the model that is used for object detection
 PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
-
-# Path to label map file
 PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
 
-# Load the label map
 with open(PATH_TO_LABELS, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
 
@@ -115,7 +100,6 @@ if labels[0] == '???':
     del (labels[0])
 
 # Load the Tensorflow Lite model.
-# If using Edge TPU, use special load_delegate argument
 if use_TPU:
     interpreter = Interpreter(model_path=PATH_TO_CKPT,
                               experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
@@ -140,26 +124,21 @@ input_std = 127.5
 # because outputs are ordered differently for TF2 and TF1 models
 outname = output_details[0]['name']
 
-if ('StatefulPartitionedCall' in outname):  # This is a TF2 model
+if 'StatefulPartitionedCall' in outname:  # This is a TF2 model
     boxes_idx, classes_idx, scores_idx = 1, 3, 0
 else:  # This is a TF1 model
     boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
-# Initialize frame rate calculation
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 
-# Initialize video stream
 videostream = VideoStream(resolution=(imW, imH), framerate=30).start()
 time.sleep(1)
 
-# for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
 while True:
 
-    # Start timer (for calculating frame rate)
     t1 = cv2.getTickCount()
 
-    # Grab frame from video stream
     frame1 = videostream.read()
 
     # Acquire frame and resize to expected shape [1xHxWx3]
@@ -172,27 +151,24 @@ while True:
     if floating_model:
         input_data = (np.float32(input_data) - input_mean) / input_std
 
-    # Perform the actual detection by running the model with the image as input
     interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
 
-    # Retrieve detection results
-    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[
-        0]  # Bounding box coordinates of detected objects
-    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]  # Class index of detected objects
-    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]  # Confidence of detected objects
+    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]
+    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]
+    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]
 
-    # Loop over all detections and draw detection box if confidence is above minimum threshold
     for i in range(len(scores)):
-        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
-            # Get bounding box coordinates and draw box
-            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+        if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
             ymin = int(max(1, (boxes[i][0] * imH)))
             xmin = int(max(1, (boxes[i][1] * imW)))
             ymax = int(min(imH, (boxes[i][2] * imH)))
             xmax = int(min(imW, (boxes[i][3] * imW)))
-            # Draw label
-            object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
+
+            center_x = int((xmin + xmax) / 2)
+            center_y = int((ymin + ymax) / 2)
+
+            object_name = labels[int(classes[i])]
             if object_name == 'person':
                 cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
                 label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
@@ -202,7 +178,10 @@ while True:
                               (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255),
                               cv2.FILLED)  # Draw white box to put label text in
                 cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0),
-                            2)  # Draw label text
+                            2)
+
+                if (10 <= center_x <= 500) and (10 <= center_y <= 400):
+                    pass
 
     cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
                 cv2.LINE_AA)
