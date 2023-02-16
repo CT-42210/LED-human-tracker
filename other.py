6d64881e6c8826cb7645c0a8156578e872914a1d
CT-42210 <nicholastroiano123@gmail.com>
2023-02-16 10:05:37 -0500

    i love nick hannah

--- other.py
+++ other.py
@@ -4,7 +4,7 @@ camera = cv2.VideoCapture(0)
 ret,frame = camera.read()
 
 while(True):
-    cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    cv2.rotate(frame, cv2.ROTATE_180)
     cv2.imshow('img1',frame)
     if cv2.waitKey(1) & 0xFF == ord('y'):
         cv2.imwrite('c1.png',frame)
