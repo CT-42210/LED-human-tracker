498c6c36887ad053fdfbb67b0c7b247b40e609d8
CT-42210 <nicholastroiano123@gmail.com>
2023-02-16 10:05:00 -0500

    i hate jacob suna god damn

--- other.py
+++ other.py
@@ -7,7 +7,7 @@ while(True):
     cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
     cv2.imshow('img1',frame)
     if cv2.waitKey(1) & 0xFF == ord('y'):
-        cv2.imwrite('images/c1.png',frame)
+        cv2.imwrite('c1.png',frame)
         cv2.destroyAllWindows()
         break
 
