341804ebaea8ec9d5ee234b93f297ccf3794c7fc
CT-42210 <nicholastroiano123@gmail.com>
2023-02-22 11:20:38 -0500

    other.py

--- other.py
+++ other.py
@@ -5,12 +5,14 @@ camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-ret, frame = camera.read()
-
 num = 1
+
 while True:
+    ret, frame = camera.read()
+
     cv2.rotate(frame, cv2.ROTATE_180)
     cv2.imshow('img1', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('y'):
         cv2.imwrite(f'/Desktop/test-images/c{num}.png', frame)
         num = num + 1
