1c7c28a4ecb1bb3a4d5044eba32b6e525595fe71
CT-42210 <nicholastroiano123@gmail.com>
2023-02-22 11:31:10 -0500

    other.py

--- other.py
+++ other.py
@@ -5,7 +5,8 @@ camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-num = 1
+with open("c-count", "r") as c_read:
+    c_num = c_read.read()
 
 while True:
     ret, frame = camera.read()
@@ -14,7 +15,7 @@ while True:
     cv2.imshow('img1', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('y'):
-        cv2.imwrite(f'/home/pi/Desktop/test-images/c{num}.png', frame)
+        cv2.imwrite(f'/home/pi/Desktop/test-images/c{c_num}.png', frame)
         num = num + 1
     elif cv2.waitKey(1) == ord('q'):
         cv2.destroyAllWindows()
