a2f6da0303edcd928a2182867f2aa8249a60d181
CT-42210 <nicholastroiano123@gmail.com>
2023-02-22 11:58:38 -0500

    other.py

--- other.py
+++ other.py
@@ -8,20 +8,19 @@ camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 with open("/home/pi/Desktop/test-images/c-count", "r") as c_read:
     c_num = c_read.read()
 
-while True:
-    ret, frame = camera.read()
+    while True:
+        ret, frame = camera.read()
 
-    cv2.rotate(frame, cv2.ROTATE_180)
-    cv2.imshow('img1', frame)
+        cv2.rotate(frame, cv2.ROTATE_180)
+        cv2.imshow('img1', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('y'):
-        cv2.imwrite(f'/home/pi/Desktop/test-images/c{c_num}.png', frame)
-        c_num = int(c_num) + 1
-        with open("/home/pi/Desktop/test-images/c-count", "w") as c_write:
+        if cv2.waitKey(1) & 0xFF == ord('y'):
+            cv2.imwrite(f'/home/pi/Desktop/test-images/c{c_num}.png', frame)
+            c_num = int(c_num) + 1
             c_read.write(str(c_num))
 
-    elif cv2.waitKey(1) == ord('q'):
-        cv2.destroyAllWindows()
-        break
+        elif cv2.waitKey(1) == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
 camera.release()
