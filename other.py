54da8f4de325809957b55590cae04d6a5f219791
CT-42210 <nicholastroiano123@gmail.com>
2023-02-22 15:00:02 -0500

    main.py

--- other.py
+++ other.py
@@ -5,26 +5,22 @@ camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-with open("/home/pi/Desktop/test-images/c-count", "r+") as c_read:
-    c_num = c_read.read()
-    print(c_num)
+c_num = 1
 
-    while True:
-        ret, frame = camera.read()
+while True:
+    ret, frame = camera.read()
 
-        cv2.rotate(frame, cv2.ROTATE_180)
-        cv2.imshow('img1', frame)
+    cv2.rotate(frame, cv2.ROTATE_180)
+    cv2.imshow('img1', frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('y'):
-            if c_num == '':
-                c_num = 1
-            c_num = int(c_num) + 1
-            cv2.imwrite(f'/home/pi/Desktop/test-images/c{c_num}.png', frame)
-            print(str(c_num))
-            c_read.write(str(c_num))
+    if cv2.waitKey(1) & 0xFF == ord('y'):
+        if c_num == '':
+            c_num = 1
+        c_num = int(c_num) + 1
+        print(str(c_num))
 
-        elif cv2.waitKey(1) == ord('q'):
-            cv2.destroyAllWindows()
-            break
+    elif cv2.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
+        break
 
 camera.release()
