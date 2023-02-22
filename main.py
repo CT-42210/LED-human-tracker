5216e68809b8e49d94240ec0abc8def968d8898e
CT-42210 <nicholastroiano123@gmail.com>
2023-02-22 14:45:40 -0500

    main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class VideoStream:
 
     def __init__(self, resolution=(640, 480), framerate=30):
         self.stream = cv2.VideoCapture(0)
-        cv2.rotate(self.stream, cv2.ROTATE_180)
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.rotate(self.stream, cv2.ROTATE_180)
         ret = self.stream.set(3, resolution[0])
         ret = self.stream.set(4, resolution[1])
 
