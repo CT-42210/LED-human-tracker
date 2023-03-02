1c2d8cca13177ed991b84bd2627605f54391e92c
CT-42210 <nicholastroiano123@gmail.com>
2023-03-02 14:51:59 -0500

    lights.py

--- lights.py
+++ lights.py
@@ -6,7 +6,8 @@ import neopixel
 pixel_pin = board.D18
 num_pixels = 200
 
-pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.3)
+pixels = neopixel.NeoPixel(pixel_pin, 200, brightness=0.3, auto_write=False)
+
 
 def color_chase(color, wait):
     for i in range(num_pixels):
@@ -15,15 +16,23 @@ def color_chase(color, wait):
         pixels.show()
     time.sleep(0.5)
 
+
 def rainbow_cycle(wait):
     for j in range(255):
-        rc_index = j * (256 // num_pixels)
         for i in range(num_pixels):
-            pixels[i] = colorwheel(rc_index + i)
-            print(i)
+            rc_index = (i * 256 // num_pixels) + j
+            pixels[i] = colorwheel(rc_index & 255)
         pixels.show()
         time.sleep(wait)
 
 
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+
 while True:
-    rainbow_cycle(0.0)
+
+    rainbow_cycle(0)
