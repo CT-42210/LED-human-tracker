dbd18bf8d7a6374f4a511333624950860131093a
CT-42210 <nicholastroiano123@gmail.com>
2023-03-02 14:24:22 -0500

    lights.poy

--- lights.py
+++ lights.py
@@ -6,15 +6,18 @@ import neopixel
 pixel_pin = board.D18
 num_pixels = 200
 
-pixels = neopixel.NeoPixel(board.D18, 200, brightness=0.3)
+pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.3)
+
 
 def rainbow_cycle(wait):
     for j in range(255):
         rc_index = j * (256 // num_pixels)
         for i in range(num_pixels):
             pixels[i] = colorwheel(rc_index + i)
+            print(i)
         pixels.show()
         time.sleep(wait)
+
 
 while True:
     rainbow_cycle(0.0)
