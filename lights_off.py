cac40e6219eb9b9a330ef16c7ae2f9f1ff46cd3d
CT-42210 <nicholastroiano123@gmail.com>
2023-03-02 15:14:40 -0500

    lights-off.py

---
+++ lights_off.py
@@ -0,0 +1,29 @@
+import time
+import board
+import neopixel
+
+pixel_pin = board.D18
+num_pixels = 200
+
+pixels = neopixel.NeoPixel(pixel_pin, 200, brightness=0.3, auto_write=False)
+
+
+def color_chase(color, wait):
+    for i in range(num_pixels):
+        pixels[i] = color
+        time.sleep(wait)
+        pixels.show()
+    time.sleep(0.5)
+
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+OFF = (0, 0, 0)
+
+while True:
+    color_chase(OFF, 0)
+    break
